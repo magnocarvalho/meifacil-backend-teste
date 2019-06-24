c1f0d9a44f556b33841c189b3679672049b4adda
Magno Carvalho <magnocarv@hotmail.com>
2019-06-24 02:10:18 -0300

    index e rotas criado

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as routes from './routes/rotas';
+
+var app = express();
+
+app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DB_HOST);
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Method', 'PUT, POST, DELETE, GET');
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    next();
+});
+
+
+app.options('*', function (req, res, next) {
+    if (req.method == 'OPTIONS')
+        res.sendStatus(200);
+});
+
+
+app.use('/api', routes);
+
+app.use(function (req, res, next) {
+    let err: any;
+    err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {}
+    });
+});
+
+var port = process.env.PORT || 1337;
+app.listen(port);
---
+++ src/routes/rotas.ts
@@ -0,0 +1,6 @@
+import * as express from 'express';
+
+var router = express.Router();
+
+
+export = router;
