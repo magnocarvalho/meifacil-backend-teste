dc341e689be9fe0d6ab7fd81ad39c63c13f3d055
Magno Carvalho <magnocarv@hotmail.com>
2019-06-24 09:49:49 -0300

    pagamento inicio da transação

--- src/controllers/PagamentoCtrl.ts
+++ src/controllers/PagamentoCtrl.ts
@@ -1,41 +1,36 @@
-import {  IPagamentoModel } from "../model/Pagamento";
+import { IPagamentoModel } from "../model/Pagamento";
 import * as async from 'async';
 import ContaCorrenteCtrl from "./ContaCorrenteCtrl";
-
-var waterfall = require('async-waterfall');
-var callback:EventListener;
+import { IContaCorrenteModel } from "../model/Contacorrente";
 
 class PagamentoCtrl {
   static create(req: { body: any; }, res: { json: (arg0: any) => void; }, next: (arg0: any) => void) {
     var obj: IPagamentoModel = req.body;
-    waterfall([
-      this.getPagador(obj, callback),
-      
-    ], function (error: any, sucesso: any) {
-      if (error) {
-        next(error);
-      } else {
-        res.json(sucesso);
-      }
+    console.log(obj);
+    var pagamento: any = {};
+    ContaCorrenteCtrl.getById(obj.pagador).then(pagador => {
+      pagamento.pagador = pagador;
+      console.log(pagamento);
+      ContaCorrenteCtrl.getById(obj.recebedor).then(recebedor => {
+        pagamento.recebedor = recebedor;
+        res.json(pagamento);
+      }, errRecebedor => {
+        next({ recebedor: errRecebedor });
+      })
+    }, errPagador => {
+      next({ pagador: errPagador });
     })
-
   };
 
-  public static getPagador(obj: any, callback: EventListener) {
-    ContaCorrenteCtrl.getById(obj.pagador).then(res => {
-      var pagador = res;
-      callback(res);
-    })
-    callback(obj);
+
+  private static getByIdPagador(id: any) {
+    return ContaCorrenteCtrl.getById(id);
   }
-  public static getRecebedor(obj: any, callback: EventListener) {
-    callback(obj);
+  private static getByIdRecebedor(id: any) {
+    return ContaCorrenteCtrl.getById(id);
   }
-  public static checkValor(obj: any, callback: EventListener) {
-    callback(obj);
-  }
-  public static valorParcelas(obj: any, callback: EventListener) {
-    callback(obj);
-  }
+
+
+
 }
 export default PagamentoCtrl;
--- src/routes/rotas.ts
+++ src/routes/rotas.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import ContaCorrenteCtrl from '../controllers/ContaCorrenteCtrl';
+import PagamentoCtrl from '../controllers/PagamentoCtrl';
 
 var router = express.Router();
 
 router.post('/conta' , ContaCorrenteCtrl.create);
+router.post('/pagar' , PagamentoCtrl.create);
 
 export = router;
